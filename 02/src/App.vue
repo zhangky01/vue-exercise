a33a1a95b8db1a752b917e393d6750248104a4e8
zhangky <iszhangky@163.com>
2022-01-13 20:08:56 +0800

    feat: 02 练习

---
+++ 02/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    {{ msg }}
+    <br/>
+    <input v-model="info" type="text"/> <button @click="handleClick">确定</button>
+    <ul>
+      <my-li v-for="item in list" :key="item">
+        <template v-slot:item="slotProps">
+          <span :style="{ color: slotProps.checked ? 'red' : 'blue' }">
+            {{ item }}
+          </span>
+        </template>
+      </my-li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import MyLi from './components/MyLi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyLi
+  },
+  data() {
+    return {
+      msg: 'hello zhangsan',
+      info: '',
+      list: []
+    }
+  },
+  methods: {
+    handleClick() {
+      this.list.push(this.info)
+      this.info = ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ 02/src/components/MyLi.vue
@@ -0,0 +1,20 @@
+<template>
+  <li class="mli">
+    <slot name="item" v-bind="{ checked }"></slot>
+    <input type="checkbox" v-model="checked" />
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      checked: false,
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
