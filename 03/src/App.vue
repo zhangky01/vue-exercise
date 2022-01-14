50c8f41f55bc10c097d05a19d64aea60b55c6409
zhangky <iszhangky@163.com>
2022-01-14 23:56:48 +0800

    feat: 03 练习

---
+++ 03/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <button @click="handleClick">新增</button>
+    <br/>
+    <!-- <MyInput v-for="(item, index) in list" :key="index"> -->
+    <MyInput v-for="item in list" :key="item">
+      <button @click="() => handleDelete(item)">删除</button> <br/>
+    </MyInput>
+  </div>
+</template>
+
+<script>
+import MyInput from './components/MyInput.vue'
+let key = 1;
+export default {
+  name: 'App',
+  components: {
+    MyInput
+  },
+  data() {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    handleClick() {
+      this.list.push(key++)
+    },
+    handleDelete(item) {
+      const index = this.list.findIndex(i => i === item)
+      console.log(index);
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ 03/src/components/MyInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input v-model="phone">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyInput',
+  data() {
+    return {
+      phone: '',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
